850b388f64d5cd995de887c0b4cfc7901990794a
unknown <krleung@697-334-Shelob.sf.k12.internal>
2014-09-25 11:16:26 -0700

    created lotso dice

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,9 +1,25 @@
-void setup()
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
+public void setup()
 {
 	noLoop();
 	size (2500,950);
 	}
-void draw()
+public void draw()
 {
 	int sum=0;
 	for (int a=0;a<2501;a+=8)
@@ -21,7 +37,7 @@ void draw()
 	textAlign(RIGHT,CENTER);
 	text(sum+" total", 1250, 475);
 }
-void mousePressed()
+public void mousePressed()
 {
 	redraw();
 }
@@ -36,13 +52,13 @@ class Die //models one single dice cube
 		b=y;
 		//variable initializations here
 	}
-	void roll()
+	public void roll()
 	{
 		num=(int)((Math.random()*6)+1);
 	
 		//your code here
 	}
-	void show()
+	public void show()
 	{
 		fill(255);
 		rect(a,b,8,8);
@@ -90,4 +106,13 @@ class Die //models one single dice cube
 	
 
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
